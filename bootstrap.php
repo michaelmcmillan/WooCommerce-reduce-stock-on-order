4d2aa9e5e64b3c63bdeb52d45f7aeadaec4bd934
Michael McMillan <email@michaelmcmillan.net>
2014-11-17 08:40:31 +0100

    plugin header added

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Reduce Stock on Order 
+ * Plugin URI: https://github.com/michaelmcmillan/WooCommerce-reduce-stock-on-order 
+ * Description: A plugin which avoids collisions in checkout by reducing stock when an order is placed. 
+ * Version: 0.0.1
+ * Author: Michael McMillan
+ * License: MIT
+ */
